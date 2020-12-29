9dc7f269871783e85dd961df6aa20e4499b3db1b
Andy <andyperp@hotmail.com>
2020-12-29 02:15:25 +0200

    last edit before trying to use it

--- chatty-bot.py
+++ chatty-bot.py
@@ -3,15 +3,17 @@
 Created on Tue Dec 29 01:36:01 2020
 
 @author: Andreas Perpyrakis
+first attemnt of creating a chat-bot plus using Git
 """
 
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+#from chatterbot.trainers import ListTrainer
 
-my_bot = ChatBot(name='PyBot', read_only=True,
+bot = ChatBot(name='PyBot', read_only=True,
                  logic_adapters=['chatterbot.logic.MathematicalEvaluation',
                                  'chatterbot.logic.BestMatch'])
 
+#standarized answers
 small_talk = ['hi there!',
               'hi!',
               'how do you do?',
@@ -33,3 +35,10 @@ math_talk_1 = ['pythagorean theorem',
                'a squared plus b squared equals c squared.']
 math_talk_2 = ['law of cosines',
                'c**2 = a**2 + b**2 - 2 * a * b * cos(gamma)']
+
+'''list_trainer = ListTrainer(my_bot)
+
+for item in (small_talk, math_talk_1, math_talk_2):
+    list_trainer.train(item) 
+'''
+    
